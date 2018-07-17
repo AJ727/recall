7fe32b4ad0c7218b2efeea267b93c1e7ce81a6ac
Austin <austin1911remo@gmail.com>
2018-07-17 17:22:54 -0400

    AddEntryPage - REFACTORED (also created mapDispatchToProps)

---
+++ src/components/AddEntryPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import DateEntryForm from './DateEntryForm';
+import { addEntry } from '../actions/content';
+
+export class AddContentPage extends React.Component {
+    onSubmit = (entry) => {
+        this.props.addEntry(entry);
+        this.props.history.push('/');
+    }
+    render(){
+        return (
+            <div>
+                <div>
+                    <h1>Add Journal Entry</h1>
+                </div>
+                <DateEntryForm 
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    addEntry : (entry) => dispatch(addEntry(entry))
+});
+
+export default connect(undefined, mapDispatchToProps)(AddContentPage);
