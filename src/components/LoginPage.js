a9a33f4aef81d719d891d4f4ff2d103d597150c0
Austin <austin1911remo@gmail.com>
2018-07-26 23:03:45 -0400

    _button, _box-layout - styled to match background

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -9,7 +9,7 @@ export const LoginPage = ({ startLogin }) => (
         <div className="box-layout__box">
             <h1 className="box-layout__title">R E C A L L</h1>
             <p>Remember the past</p>
-            <button className="button" onClick={startLogin}>Login with Google</button>
+            <button className="button--particles" onClick={startLogin}>Login with Google</button>
         </div>
        
     </div>
