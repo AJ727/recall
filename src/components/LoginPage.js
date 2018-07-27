3ba627bda89fc06817184aaa7e47102ac040bec8
Austin <austin1911remo@gmail.com>
2018-07-26 22:46:14 -0400

    Installed particles.js/implemented it
    First push of using particles, it's not styled correctly or anything just yet

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
+import Particles from 'react-particles-js';
 
 export const LoginPage = ({ startLogin }) => (
-    <div className="box-layout">
+    <div>
+        <Particles className="particles-layout" />
         <div className="box-layout__box">
             <h1 className="box-layout__title">R E C A L L</h1>
             <p>Remember the past</p>
             <button className="button" onClick={startLogin}>Login with Google</button>
         </div>
+       
     </div>
 );
 
