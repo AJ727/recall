cdbcf0e92187335820c1883ca77f71357e38976c
Austin <austin1911remo@gmail.com>
2018-07-26 23:20:13 -0400

    LoginPage - styled particles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,13 +5,35 @@ import Particles from 'react-particles-js';
 
 export const LoginPage = ({ startLogin }) => (
     <div>
-        <Particles className="particles-layout" />
+        {/* I think this can also be defined in the json file */}
+        <Particles 
+            className="particles-layout" 
+            params= {
+                {
+                    particles: {
+                        number: {
+                            value: 50,
+                            density: {
+                                value_area: 1000
+                            }
+                        },
+                        shape: {
+                            stroke: {
+                                width: 4
+                            }
+                        },
+                        line_linked: {
+                            width: .7
+                        }
+                    }
+                }
+            }
+        />
         <div className="box-layout__box">
             <h1 className="box-layout__title">R E C A L L</h1>
             <p>Remember the past</p>
             <button className="button--particles" onClick={startLogin}>Login with Google</button>
         </div>
-       
     </div>
 );
 
