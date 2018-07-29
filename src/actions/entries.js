0ca38d2e13543e0307b033751cf591455b1f6eb9
Austin Pickart <austin1911remo@gmail.com>
2018-07-29 00:11:16 -0400

    entries.js - startAddEntry written

--- src/actions/entries.js
+++ src/actions/entries.js
@@ -1,6 +1,6 @@
 import uuid from 'uuid';
+import database from '../firebase/firebase';
 
-// Restructuring this data because it was garbage before
 export const addEntry = ({date = '', entry = ''} = {}) => ({
     type: 'ADD_ENTRY',
     entryObj: {
@@ -9,6 +9,27 @@ export const addEntry = ({date = '', entry = ''} = {}) => ({
         entry
     }
 });
+
+export const startAddEntry = (entryData = {}) => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        const {
+            date = '',
+            entry = ""
+        } = entryData
+
+        const entry = {date, entry};
+        return database.ref(`users/${uid}/entries`).push(entry)
+            .then((ref) => {
+                dispatch(addEntry({
+                    id: ref.key,
+                    ...entry
+                }))
+            })
+
+        )
+    }
+};
 
 export const editEntry = (id, updates) => ({
     type: 'EDIT_ENTRY',
