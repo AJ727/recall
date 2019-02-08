d7b5e606e6f090c2f6832971f0f9878e73e06da8
Austin Pickart <austin1911remo@gmail.com>
2019-02-08 12:16:08 -0500

    text-selector.js - commenting

--- src/selectors/text-selector.js
+++ src/selectors/text-selector.js
@@ -1,11 +1,8 @@
 export default (entries, {text}) => {
     // filter returns a new array, so if a condition passes, the item is good and is returned
-    return entries.filter((entryObj) => {
-        // CODE FOR MATCHING TEXT - 
+    return entries.filter((entryObj) => { 
         const textMatch = entryObj.entry.toLowerCase().includes(text.toLowerCase());
-        
-        // If true, return a new array with this specific entryObj,
-        // and continue to the next object
+        // If true, return a new array with this specific entryObj, and continue to the next object
         return textMatch;    
     }).sort((a,b) => {
         // compare function that sorts from newest to oldest
