8cd033d73bf1558c5f56386f770284ecf8bc2510
Austin <austin1911remo@gmail.com>
2019-12-07 19:43:10 -0500

    Undid the garbage changes I had done a while back

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export const Header = ({ startLogout }) => (
     <header className="header">
     <div className="content-container">
         <div className="header__content">
-            <Link className="header__title header--recall" to="/dashboard">
+            <Link className="header__title" to="/dashboard">
                 <h1>recall</h1>
             </Link>
             <Link className="header__title" to="/add">
