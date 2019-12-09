352bfabf94a40a81ab3415952ef14406660c14c0
Austin <austin1911remo@gmail.com>
2019-12-08 23:50:16 -0500

    Converted selectors

--- src/selectors/entries.ts
+++ src/selectors/entries.ts
@@ -1,13 +1,14 @@
-import moment from 'moment';
+import * as moment from 'moment';
+import { Entry } from '../interfaces/Actions';
 
-export default (entries, {date}) => {
+export default (entries: Entry[], {date}: any): Entry[] => {
     // filter returns a new array, so if a condition passes, the item is good and is returned
-    return entries.filter((entryObj) => {
+    return entries.filter((entryObj: Entry): moment.Moment => {
         
         // CODE FOR MATCHING DATE - Call moment on the object's date
-        const dateChosen = moment(entryObj.date);
+        const dateChosen: moment.Moment = moment(entryObj.date);
         // If there is a date, and that date is the same day, return true
-        const dateFiltered = date ? date.isSame(dateChosen, 'day') : true;
+        const dateFiltered: moment.Moment = date ? date.isSame(dateChosen, 'day') : true;
         
         // If true, return a new array with this specific entryObj,
         // and continue to the next object
--- src/selectors/text-selector.ts
+++ src/selectors/text-selector.ts
@@ -1,10 +1,12 @@
-export default (entries, {text}) => {
+import { Entry } from "../interfaces/Actions";
+
+export default (entries: Entry[], {text}: any): Entry[] => {
     // filter returns a new array, so if a condition passes, the item is good and is returned
-    return entries.filter((entryObj) => { 
-        const textMatch = entryObj.entry.toLowerCase().includes(text.toLowerCase());
+    return entries.filter((entryObj: Entry): boolean => { 
+        const textMatch: boolean = entryObj.entry.toLowerCase().includes(text.toLowerCase());
         // If true, return a new array with this specific entryObj, and continue to the next object
         return textMatch;    
-    }).sort((a,b) => {
+    }).sort((a: Entry, b: Entry): number => {
         // compare function that sorts from newest to oldest
         // if a's date is newer than b's, return true
         return a.date < b.date ? 1 : -1;
